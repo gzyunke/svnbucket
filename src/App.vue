c88dad65ef2a1408bdb990eb148466de3dc00924
nate <gzyunke@qq.com>
2018-11-27 21:16:12 +0800

    修复报错

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,25 @@
 
 <script>
     export default {
-        methods: {},
+        methods: {
+            statsConfig()
+            {
+                var _mtac = {"performanceMonitor":1,"senseQuery":1};
+                (function() {
+                    var mta = document.createElement("script");
+                    mta.src = "https://pingjs.qq.com/h5/stats.js?v2.0.4";
+                    mta.setAttribute("name", "MTAH5");
+                    mta.setAttribute("sid", "500553641");
+                    mta.setAttribute("cid", "500590386");
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(mta, s);
+                })();
+            },
+        },
         mounted()
-        {}
+        {
+            this.statsConfig(0)
+        }
     }
 </script>
 
